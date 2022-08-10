a01684d5c2d7151e5b496cdb3049b6e6bcf895fe
kimjungjin <jungks9351@gmail.com>
2022-08-10 19:01:51 +0900

    tailwind css 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <h1>Hello Next.js</h1>
+  return (
+    <>
+      <h1 className="text-9xl underline text-red-600">Hello Next.js</h1>
+    </>
+  )
 }
 
 export default Home
